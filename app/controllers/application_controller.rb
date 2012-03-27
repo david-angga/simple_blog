61c981c5bc944a25466c63fca82dad6f9a1d7c65
David Angga Prasetya <carsmetic.oc@gmail.com>
2012-03-27 22:47:53 +0700

    Fix active_admin reorder bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
-  before_filter :populate_categories_and_tags
-  
-  def populate_categories_and_tags
-    @categories = Category.all
-    @tags = Tag.all
-  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     @posts = Post.all
+    @categories = Category.all
   end
 end
